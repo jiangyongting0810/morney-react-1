a8700420fc1559ebf85c11e56e59ccacaa2c1288
YongTing <jiangyongting0810@gmail.com>
2022-09-25 11:24:08 +0800

    创建 views 目录

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,12 @@ import {
     Route,
     Redirect
 } from 'react-router-dom';
-import Layout from 'components/Layout';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import NoMatch from './views/NoMatch';
+import Tags from './views/Tags';
 
-
-
-export default function App() {
+function App() {
     return (
         <Router>
             <Switch>
@@ -31,34 +32,10 @@ export default function App() {
         </Router>
     );
 }
+export default App;
 
-function NoMatch() {
-    return (
-        <div>页面不存在</div>
-    );
-}
 
-function Statistics() {
-    return (
-        <Layout>
-            <h2>统计页面</h2>
-        </Layout>
-    );
 
-}
 
-function Tags() {
-    return (
-        <Layout>
-            <h2>标签页面</h2>
-        </Layout>
-    );
-}
 
-function Money() {
-    return (
-        <Layout>
-            <h2>记账页面</h2>
-        </Layout>
-    );
-}
+
